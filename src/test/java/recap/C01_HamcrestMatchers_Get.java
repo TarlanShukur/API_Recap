4650240a9d9e99ce76c705efa4da9881b5dabe38
compusta.az <tarlanshukur@gmail.com>
2024-04-26 07:36:24 +0400

    API_Recap

--- src/test/java/recap/C01_HamcrestMatchers_Get.java
+++ src/test/java/recap/C01_HamcrestMatchers_Get.java
@@ -12,16 +12,18 @@ public class C01_HamcrestMatchers_Get {
     @Test
     public void hamcrestMatchers_Get() {
         // 1. Set the URL
-        String url = "https://petstore.swagger.io/v2/pet/123";
+        String url = "https://petstore.swagger.io/v2/pet/124";
 
         given().when().get(url)
                 .then()
                 .statusCode(200)
                 .contentType(ContentType.JSON)
-                .body("name",containsString("doggie"))
+                .body("name",containsString("Test1"))
                 .body("status",equalTo("available"))
-                .body("category.name",equalTo("string"))
-                .body("tags[0].name",equalTo("string"))
+                .body("category.name",equalTo("Test"))
+                .body("tags[0].name",equalTo("234"))
+                .body("tags.find{it.id==0}.name",equalTo("234"))
+                .log().body()
         ;
     }
 }
