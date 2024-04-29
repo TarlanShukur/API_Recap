52232bc9e698cf3b1e2c0821578c3b53b7e09358
compusta.az <tarlanshukur@gmail.com>
2024-04-29 19:31:53 +0400

    TRL

---
+++ src/test/java/test/API_GetTequest.java
@@ -0,0 +1,30 @@
+package test;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.json.JSONObject;
+import org.junit.Test;
+
+import static io.restassured.path.json.JsonPath.given;
+
+public class API_GetTequest {
+
+    @Test
+    public void get01() {
+        String url = "https://jsonplaceholder.typicode.com/posts/44";
+
+        JSONObject expBody = new JSONObject();
+
+        expBody.put("userId",5);
+        expBody.put("title","optio dolor molestias sit");
+
+        RestAssured.given().when().get(url);
+
+
+    }
+
+
+
+
+
+}
